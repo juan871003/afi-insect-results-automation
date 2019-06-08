d3227d4883bf3af2eba6f9ae879e9ccc0357eb55
Juan Castano <juan871003@gmail.com>
2019-06-08 21:26:31 +1000

    m

--- routes/entryprocessor.js
+++ routes/entryprocessor.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const entryNr = req.query.entry;
+  
+  const puppeteer = require('puppeteer');
+  // const env = process.env.USERNAME === "juan8" ? "dev" : "prod";
+  /* (async () => {
+    const browser = await puppeteer.launch({ executablePath: '/usr/bin/google-chrome-stable', headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.example.com'); await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    browser.close();
+  })(); */
+
+
   res.send({
     entryNr: entryNr,
     entryStatus: 'processing...'
