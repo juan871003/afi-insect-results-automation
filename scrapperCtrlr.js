357f0cb6e74bfbfc9d7055e1ff04a52b92119363
Juan Castano <juan871003@gmail.com>
2019-06-15 16:31:04 +1000

    scrappig status as test

--- scrapperCtrlr.js
+++ scrapperCtrlr.js
@@ -51,15 +51,57 @@ async function checkEntriesResultsLoop() {
 }
 
 async function scrap(entriesToScrapp) {
-  await sleep(1000);
-  // TODO: don't modify entries, just return result.
-  entriesToScrapp.forEach( entryNr => entries.set(entryNr, 'Test Done!'));
-
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  await gotoMainPage(page);
+  for(const entryNr of entriesToScrapp) {
+    const newPage = await getEntryPage(page, entryNr);
+    const status = await getEntryStatus(newPage);
+    entries.set(entryNr, status);
+    await newPage.close();
   }
+  await page.waitFor(1000);
+  await browser.close();
 }
 
+async function gotoMainPage(page) {
+  await page.goto('https://apps.daff.gov.au/BrokerReports/ASP/Login.asp');
+  await page.click('#username');
+  await page.keyboard.type('com043707');
+  await page.click('#password');
+  await page.keyboard.type('19rohini');
+  await page.click('input[name="btnSubmit"]');
+  await page.goto('https://apps.daff.gov.au/BrokerReports/asp/SingleEntry.asp');
+}
 
+async function getEntryPage(page, entryNr) {
+  await page.waitForSelector('#txtEntry');
+  await page.click('#txtEntry', {clickCount: 3});
+  await page.keyboard.type(entryNr);
+  // wait for target created for 30 seconds, not forever.
+  const newPagePromise =
+    Promise.race([
+      new Promise(x => page.browser().once('targetcreated', target => x(target.page()))),
+      waitUntil(30),
+    ]);
+  await page.click('input[name="btnSubmit"]');
+  return await newPagePromise;
+}
+
+async function waitUntil(seconds) {
+  return new Promise((resolve, reject) => {
+    const wait = setTimeout(() => {
+      clearTimeout(wait);
+      reject(new Error(`timeout reached, waiting for ${seconds} seconds`));
+    }, seconds * 1000);
+  });
+}
+
+async function getEntryStatus(newPage) {
+  const statusXpath = '//td/b[text()="Status:"]/../following-sibling::td[1]';
+  await newPage.waitForXPath(statusXpath);
+  const statusHandle = await newPage.$x(statusXpath);
+  const status = await newPage.evaluate(statusEl => statusEl.textContent, statusHandle[0]);
+  await disposeHandle(statusHandle);
+  return status;
+}
 
 module.exports = scrapperCtrl;
